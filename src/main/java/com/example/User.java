5d27a37cb98951278ec2dd65b58b2dd4078cc837
AmirSaleh-K-N <amirnaeini2007@gmail.com>
2026-06-04 17:36:52 +0330

    Add User class with fields, getters, setters, and constructor and methods for adding and removing pictures and albums

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,85 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private boolean isBanned = false;
+    private String name;
+    private String pssword;
+    private boolean isLoggedOut;
+    private List<Picture> pictures = new ArrayList<>();
+    private List<Album> albums = new ArrayList<>();
+
+    public User(String name, String pssword, boolean isLoggedOut, List<Picture> pictures, List<Album> albums) {
+        this.name = name;
+        this.pssword = pssword;
+        this.isLoggedOut = isLoggedOut;
+        this.pictures = pictures;
+        this.albums = albums;
+    }
+
+    public boolean isBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(boolean isBanned) {
+        this.isBanned = isBanned;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPssword() {
+        return pssword;
+    }
+
+    public void setPssword(String pssword) {
+        this.pssword = pssword;
+    }
+
+    public boolean isLoggedOut() {
+        return isLoggedOut;
+    }
+
+    public void setLoggedOut(boolean isLoggedOut) {
+        this.isLoggedOut = isLoggedOut;
+    }
+
+    public List<Picture> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<Picture> pictures) {
+        this.pictures = pictures;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
+    //
+    public void addPicture(Picture picture) {
+        pictures.add(picture);
+    }
+
+    public void addAlbum(Album album) {
+        albums.add(album);
+    }
+
+    public void removePicture(Picture picture) {
+        pictures.remove(picture);
+    }
+
+    public void removeAlbum(Album album) {
+        albums.remove(album);
+    }
+}
