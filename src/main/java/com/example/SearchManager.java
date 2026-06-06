d4e1da2dc8ed4420746d3c6ecd4a703917c2eea7
AmirSaleh-K-N <amirnaeini2007@gmail.com>
2026-06-06 21:50:50 +0330

    The SearchManager class is almost finished whit different search,filter and sort methods

--- src/main/java/com/example/SearchManager.java
+++ src/main/java/com/example/SearchManager.java
@@ -1,8 +1,9 @@
 package com.example;
 
-import java.time.LocalDate;
+import java.time.*;
 import java.util.*;
 import java.util.stream.Collectors;
+
 
 public class SearchManager {
     private SearchManager() {
@@ -11,27 +12,27 @@ public class SearchManager {
     public static List<Picture> searchByAlbum(String albumName, User user) {
         return user.getPictures().stream()
                 .filter(picture -> picture.getAlbums().stream()
-                        .anyMatch(album -> album.getName().toLowerCase().contains(albumName)))
+                        .anyMatch(album -> album.getName().toLowerCase().contains(albumName.toLowerCase())))
                 .collect(Collectors.toList());
     }
 
     public static List<Picture> searchByName(String pictureName, User user) {
         return user.getPictures().stream()
-                .filter(picture -> picture.getName().toLowerCase().contains(pictureName))
+                .filter(picture -> picture.getName().toLowerCase().contains(pictureName.toLowerCase()))
                 .collect(Collectors.toList());
     }
 
     public static List<Picture> searchByComment(String commentText, User user) {
         return user.getPictures().stream()
                 .filter(picture -> picture.getComments().stream()
-                        .anyMatch(comment -> comment.getText().toLowerCase().contains(commentText)))
+                        .anyMatch(comment -> comment.getText().toLowerCase().contains(commentText.toLowerCase())))
                 .collect(Collectors.toList());
     }
 
     public static List<Picture> searchByTag(String tagSearch, User user) {
         return user.getPictures().stream()
                 .filter(picture -> picture.getTags().stream()
-                        .anyMatch(tag -> tag.toLowerCase().contains(tagSearch)))
+                        .anyMatch(tag -> tag.toLowerCase().contains(tagSearch.toLowerCase())))
                 .collect(Collectors.toList());
     }
 
@@ -40,5 +41,34 @@ public class SearchManager {
                 .filter(picture -> picture.getTimeOfAdd().toLocalDate().equals(localDate))
                 .collect(Collectors.toList());
     }
-    
+
+    public static List<Picture> sortByDateTime(User user) {
+        return user.getPictures().stream()
+                .sorted((picture1, picture2) -> picture2.getTimeOfAdd().compareTo(picture1.getTimeOfAdd()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Picture> sortByName(User user) {
+        return user.getPictures().stream()
+                .sorted((picture1, picture2) -> picture1.getName().compareTo(picture2.getName()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Picture> sortByLikes(User user) {
+        return user.getPictures().stream()
+                .sorted((picture1, picture2) -> Integer.compare(picture2.getLikes(), picture1.getLikes()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Picture> filterMoreThanLikes(User user, int likes) {
+        return user.getPictures().stream()
+                .filter(picture -> picture.getLikes() > likes)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Picture> filterAfterThisDate(User user, LocalDate date) {
+        return user.getPictures().stream()
+                .filter(picture -> picture.getTimeOfAdd().toLocalDate().isAfter(date))
+                .collect(Collectors.toList());
+    }
 }
