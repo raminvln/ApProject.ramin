34e1dcdd4121cbc267ae80ca6f00b7f9b4f76e6d
AmirSaleh-K-N <amirnaeini2007@gmail.com>
2026-07-09 17:13:22 +0330

    Problems was edited.

--- album_frontend/android/app/build.gradle.kts
+++ album_frontend/android/app/build.gradle.kts
@@ -12,18 +12,16 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.example.album_frontend"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
     }
 }
 
