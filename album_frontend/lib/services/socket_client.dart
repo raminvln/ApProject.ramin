4a2607af043aea7e3a4d10b149adf3dc2d4a5d16
raminvln <raminvalinezhad2016@gmail.com>
2026-08-05 13:08:35 +0330

    replacing the existing send() method

--- album_frontend/lib/services/socket_client.dart
+++ album_frontend/lib/services/socket_client.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'dart:async';
 
 class SocketClient {
   static Future<Map<String, dynamic>> send({
@@ -9,6 +10,7 @@ class SocketClient {
     required Map<String, dynamic> payload,
   }) async {
     final socket = await Socket.connect('YOUR_SERVER_IP', 8080);
+
     final request = jsonEncode({
       'method': method,
       'userName': userName,
@@ -16,24 +18,35 @@ class SocketClient {
       'payload': payload,
     });
     socket.write(request);
-    socket.write('\n'); 
-    final response = await socket.first; 
+    socket.write('\n');
+    await socket.flush();
+
+    final completer = Completer<String>();
+    final buffer = StringBuffer();
+    socket.transform(utf8.decoder).listen(
+      (data) => buffer.write(data),
+      onDone: () => completer.complete(buffer.toString()),
+      onError: (e) => completer.completeError(e),
+    );
+
+    final raw = await completer.future.timeout(const Duration(seconds: 10));
     socket.destroy();
-    return jsonDecode(utf8.decode(response));
+    return jsonDecode(raw) as Map<String, dynamic>;
   }
-  
+
   static Future<Map<String, dynamic>> register({
-  required String userName,
-  required String password,
-}) async {
-  return await send(
-    method: "POST",
-    userName: userName,
-    route: "/register",
-    payload: {
-      "userName": userName,
-      "password": password,
-    },
-  );
-}
+    required String username,
+    required String password,
+  }) async {
+    return await send(
+      method: "POST",
+      userName: username,
+      route: "/register",
+      payload: {
+        "displayName": username,
+        "userName": username,
+        "password": password,
+      },
+    );
+  }
 }
